0becdafb6f6a694f1742fd55ff4bba72e53bbc93
Miftahudin Mohammed <miftahadinmohamed99@gmail.com>
2026-08-09 22:17:59 +0300

    Add maximumScore function to compute subarray score
    The function calculates the maximum score of a good subarray by expanding from the index 'k' and maintaining the minimum value within the current window.

---
+++ Maximum Score of a Good Subarray.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maximumScore(self, nums: List[int], k: int) -> int:
+        left = k
+        right = k
+
+        minimum = nums[k]
+        answer = nums[k]
+
+        while left > 0 or right < len(nums) - 1:
+            if left == 0:
+                right += 1
+            elif right == len(nums) - 1:
+                left -= 1
+            elif nums[left - 1] >= nums[right + 1]:
+                left -= 1
+            else:
+                right += 1
+
+            minimum = min(minimum, nums[left], nums[right])
+            length = right - left + 1
+            score = minimum * length
+            answer = max(answer, score)
+
+        return answer
